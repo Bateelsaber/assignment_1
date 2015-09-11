e5f05c521d7df755284372bf16da246e7088d62e
Bateelsaber <sabeb234@newschool.edu>
2015-09-11 06:43:43 +0300

    Draw image

--- sketch.js
+++ sketch.js
@@ -11,15 +11,143 @@ function draw() {
   background(0);
 	image(crab, 0, 0);
 	// set a fill color white
-	fill(255, 255, 255);
+	fill(33);
+	// draw a circle
+	ellipse(320, 240, 600, 110);
+
+	// set a fill color white
+	fill(117, 84, 27);
 	// draw ground
 	noStroke();
-	rect(0, 300, 640, 100);
+
+ellipse(80, 215, 15, 15);
+ellipse(217, 218, 15, 15);
+ellipse(570, 210, 15, 15);
+ellipse(420, 217, 15, 15);
+ellipse(105, 230, 15, 15);
+ellipse(245, 233, 15, 15);
+ellipse(540, 227, 15, 15);
+ellipse(448, 232, 15, 15);
+
+ellipse(135, 236, 15, 15);
+ellipse(195, 229, 15, 15);
+ellipse(510, 235, 15, 15);
+ellipse(475, 237, 15, 15);
+ellipse(165, 236, 15, 15);
+ellipse(275, 250, 15, 15);
+ellipse(375, 245, 15, 15);
+ellipse(402, 227, 15, 15);
+ellipse(345, 255, 15, 15);
+ellipse(312, 257, 15, 15);
+
+	// set a fill color white
+	fill(71, 75, 33);
+	// draw ground
+	noStroke();
 
 	// set a stroke color
 
 	// draw a circle
-	ellipse(320, 280, 100, 100);
-	ellipse(320, 200, 80, 80);
-	ellipse(320, 140, 60, 60);
+	ellipse(150, 180, 180, 110);
+  ellipse(490, 180, 180, 110);
+	ellipse(320, 200, 180, 110);
+	ellipse(110, 105, 30, 30);
+	ellipse(180, 105, 30, 30);
+	ellipse(450, 105, 30, 30);
+	ellipse(520, 105, 30, 30);
+	ellipse(280, 125, 33, 33);
+	ellipse(350, 125, 33, 33);
+
+	rect(106, 110, 8, 50);
+	rect(175, 110, 9, 50);
+	rect(448, 110, 8, 50);
+	rect(515, 110, 9, 50);
+	rect(275, 120, 11, 50);
+	rect(345, 120, 11, 50);
+
+	ellipse(40, 100, 55, 55);
+	ellipse(595, 100, 55, 55);
+	ellipse(70, 150, 20, 20);
+	ellipse(560, 145, 20, 20);
+	ellipse(60, 135, 30, 30);
+	ellipse(570, 130, 30, 30);
+
+	// set a fill color white
+	fill(69, 46, 17);
+	// draw ground
+	noStroke();
+
+	// set a stroke color
+
+	// draw a circle
+	ellipse(150, 180, 165, 90);
+  ellipse(490, 180, 165, 90);
+	ellipse(320, 200, 165, 90);
+
+
+		ellipse(40, 100, 45, 45);
+		ellipse(595, 100, 45, 45);
+		ellipse(70, 150, 10, 10);
+		ellipse(560, 145, 10, 10);
+		ellipse(60, 135, 20, 20);
+		ellipse(570, 130, 20, 20);
+
+		// set a fill color white
+		fill(71, 75, 33);
+		// draw ground
+		noStroke();
+
+		// set a stroke color
+		// draw a circle
+		ellipse(150, 176, 169, 87);
+		ellipse(490, 176, 169, 87);
+		ellipse(320, 196, 169, 87);
+
+
+			ellipse(40, 95, 45, 45);
+			ellipse(595, 95, 45, 45);
+			ellipse(70, 144, 8, 8);
+			ellipse(560, 139, 8, 8);
+			ellipse(60, 130, 18, 18);
+			ellipse(570, 125, 18, 18);
+
+			// set a fill color white
+			fill(237, 233, 173);
+			// draw ground
+			noStroke();
+
+			ellipse(110, 105, 25, 25);
+			ellipse(180, 105, 25, 25);
+			ellipse(450, 105, 25, 25);
+			ellipse(520, 105, 25, 25);
+			ellipse(280, 125, 28, 28);
+			ellipse(350, 125, 28, 28);
+
+			// set a fill color white
+			fill(0);
+			// draw ground
+			noStroke();
+
+			ellipse(111, 103, 5, 5);
+			ellipse(181, 104, 5, 5);
+			ellipse(452, 106, 5, 5);
+			ellipse(522, 105, 5, 5);
+			ellipse(281, 125, 6, 6);
+			ellipse(350, 125, 6, 6);
+
+			rect(0, 60, 50, 50)
+      rect(590, 60, 50, 50)
+
+			// set a fill color white
+			stroke(45, 46, 13);
+strokeWeight(8);
+line( 100, 90, 120, 100);
+line( 440, 90, 460, 100);
+line( 270, 105, 290, 115);
+
+line( 190, 90, 170, 100);
+line( 530, 90, 510, 100);
+line( 360, 105, 340, 115);
+
+
 }
